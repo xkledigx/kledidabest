34c0a8b457a1ed88089d32c5616d50b8279b5e6e
Kledi <134799858+xkledigx@users.noreply.github.com>
2023-05-27 20:38:31 +0200

    pleasedonateme.lua

--- pleasedonateme.lua
+++ pleasedonateme.lua
@@ -1,10 +1,10 @@
 local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/cypherdh/VanisUILIB/main/.gitignore'))()
 
-local Window = library:CreateWindow("Name", "Version", 10044538000)
+local Window = library:CreateWindow("Kledi", "dabest", 10044538000)
 
 local Tab = Window:CreateTab("Scripts")
 
-local Page = Tab:CreateFrame("Page 1")
+local Page = Tab:CreateFrame("main")
 
 Button = Page:CreateButton("Button", "Description", function()
 CreateNotification("Title", "Description", function(value)
