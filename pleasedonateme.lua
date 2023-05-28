f72224db638eb608f5b89810a0943e51ac03a1be
Kledi <134799858+xkledigx@users.noreply.github.com>
2023-05-28 10:53:28 +0200

    pleasedonateme.lua

--- pleasedonateme.lua
+++ pleasedonateme.lua
@@ -68,26 +68,6 @@ localButton = Page:CreateButton("Auto Claim Booth"), "Claim Booth Automaticaly",
     findUnclaimed = value
     claim=value
 
-local function findUnclaimed()
-    for i, v in pairs(Players.LocalPlayer.PlayerGui.MapUIContainer.MapUI.BoothUI:GetChildren()) do
-        if (v.Details.Owner.Text == "unclaimed") then
-            table.insert(unclaimed, tonumber(string.match(tostring(v), "%d+")))
-        end
-    end
-end
-if not pcall(findUnclaimed) then
-    serverHop()
-end
-
-local claimCount = #unclaimed
-
-local Page = Tab:CreateFrame("Kledi")
-
-localButton = Page:CreateButton("BoothClaim"), "Claim Booth Automaticaly", function(boothclaim)
-    Button:UpdateButton("New Title", "New Description")
-    findUnclaimed = value
-    boothclaim = claim
-
 local function boothclaim()
     require(game.ReplicatedStorage.Remotes).Event("ClaimBooth"):InvokeServer(unclaimed[1])
     if not string.find(Players.LocalPlayer.PlayerGui.MapUIContainer.MapUI.BoothUI:FindFirstChild(tostring("BoothUI".. unclaimed[1])).Details.Owner.Text, Players.LocalPlayer.DisplayName) then
@@ -96,47 +76,5 @@ local function boothclaim()
             error()
         end
     end
-end
-
---Checks if booth claim fails
-while not pcall(boothclaim) do
-    if errCount >= claimCount then
-        serverHop()
-    end
-    table.remove(unclaimed, 1)
-    errCount = errCount + 1
-end
-
-hopSet()
-
-local Page = Tab:CreateFrame("main")
-
-local Toggle = Page:CreateToggle("WALK TO BOTH"), "Claim Booth Automaticaly", function(Value)
-    Toggle:UpdateToggle("New Title", "New Description")
-
---walks to booth
-game:GetService('VirtualInputManager'):SendKeyEvent(true, "LeftControl", false, game)
-local Controls = require(Players.LocalPlayer.PlayerScripts:WaitForChild("PlayerModule")):GetControls()
-Controls:Disable()
-Players.LocalPlayer.Character.Humanoid:MoveTo(Vector3.new(booths[tostring(unclaimed[1])]:match("(.+), (.+), (.+)")))
-local atBooth = false
-local function noclip()
-    for i,v in pairs(Players.LocalPlayer.Character:GetDescendants()) do
-        if v:IsA("BasePart") then
-            v.CanCollide = false
-        end
-    end
-end
-local noclipper = game:GetService("RunService").Stepped:Connect(noclip)
-Players.LocalPlayer.Character.Humanoid.MoveToFinished:Connect(function(reached)
-    atBooth = true
-end)
-while not atBooth do
-    task.wait(.1)
-    if Players.LocalPlayer.Character.Humanoid:GetState() == Enum.HumanoidStateType.Seated then
-        Players.LocalPlayer.Character.Humanoid.Jump = true
-    end
-end
-end
 end
 end
